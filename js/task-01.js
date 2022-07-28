96468b615bba069f32369afecfcfa1edfa16de9d
N3cker <piaszuu@gmail.com>
2022-07-28 21:28:04 +0200

    update

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const categories = document.getElementById('categories');
-const categoriesItems = categories.querySelectorAll('.item');
-
-console.log('Number of categories:', categoriesItems.length);
-
-categoriesItems.forEach(item => {
-    console.log('Category:', item.querySelector('h2').textContent);
-    console.log('Elements', item.querySelectorAll('li').length);
-});
